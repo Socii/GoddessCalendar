5bc4f90c5dd1e74399ec5a9a7f726ede6fe9165b
Socii Watts <sociiw@gmail.com>
2019-09-12 13:22:37 +0100

    Initial Commit

---
+++ GoddessCalendar/GoddessCalendar.h
@@ -0,0 +1,13 @@
+// GoddessCalendar
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GoddessCalendar.
+FOUNDATION_EXPORT double GoddessCalendarVersionNumber;
+
+//! Project version string for GoddessCalendar.
+FOUNDATION_EXPORT const unsigned char GoddessCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoddessCalendar/PublicHeader.h>
+
+
